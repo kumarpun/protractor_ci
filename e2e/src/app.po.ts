ca6f3dbe0074ee60352c798bae04d9b55d174af9
Apple <apple@Apples-MacBook-Pro.local>
2021-06-28 00:08:55 +0545

    test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,10 +2,10 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/https://class.digiisocial.com/anz/?demo');
   }
 
   getParagraphText() {
-    return element(by.css('app-root jbjbh1')).getText();
+    return element(by.css('app-root h1')).getText();
   }
 }
