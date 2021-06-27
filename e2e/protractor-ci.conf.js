fed0288d80e7307c95cc9814ae3117bf379cc31a
Apple <apple@Apples-MacBook-Pro.local>
2021-06-28 00:40:20 +0545

    test

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -2,9 +2,9 @@ const config = require('./protractor.conf').config;
 
 config.capabilities = {
   browserName: 'chrome',
-  'chromeOptions': {
-    args: ["--start-maximized", "--window-size=1630,963"]
-    // binary: require('puppeteer').executablePath(),
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox', '--disable-gpu'],
+    binary: require('puppeteer').executablePath(),
   },
 };
 
