18e9b354fd1fe643d79001cb051379cbc1c33191
Apple <apple@Apples-MacBook-Pro.local>
2021-06-28 13:40:26 +0545

    proof of concept

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -28,7 +28,7 @@ fdescribe('Post Section', () => {
 
    it('Verify user can post a text on messageboard', async () => {
 
-    await app.login.textAreaForPost.sendKeys('helloooo from automation');
+    await app.login.textAreaForPost.sendKeys('helloooo from github action');
        await app.login.PostButton.click();
        browser.sleep(5000);
        
